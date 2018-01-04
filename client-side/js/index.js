8e79bde71e63a249c48f22fa6f3dcaabea76c806
Haokun Chen <chenhk@live.unc.edu>
2018-01-04 21:12:16 +0800

    first commit

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,35 +1,22 @@
-function checkTop(){
-  if($(window).scrollTop() >=50 ){
-    $('#navbar-example').addClass("not-at-top").removeClass("at-top");
-  }else{
-    $('#navbar-example').addClass("at-top").removeClass("not-at-top");
-  }
-}
-
 $(document).ready(function(){
-    checkTop();
-  });
 
-$("#navbar-example a").on('click', function(event) {
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
-    // Prevent default anchor click behavior
-    event.preventDefault();
-    // Store hash
-    var hash = this.hash;
-
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, 'easeInCubic', function(){
-
-    // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-
-  } // End if
-
-});
-
-$(window).scroll(function() {
-  checkTop();
+  $.ajax({
+        type: 'GET',
+        url: "../server-side/block.php",
+        data: {
+          "action": "all"
+        },
+        datatype: "json",
+        cache: false,
+        success: function(json){
+          var blocks = json.slice(0,6);
+          load_blocks(blocks, "#blocks_showcase_array", "#blocks_showcase_modal", 0);
+          console.log(blocks);
+          // load_block_pic(json[0],"#blocks_array",);
+          // load_block_form(json[0]);
+        },
+        error: function(xhr,status){
+          alert("errors loading index page");
+        }
+      });
 });
